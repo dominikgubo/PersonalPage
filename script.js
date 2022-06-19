9cc00cc707158a93d0d82f5f70b19cf3c16b567e
Gubinator <gubo.dominik@gmail.com>
2022-06-19 19:28:28 +0200

    Hamburger button, responsive

--- script.js
+++ script.js
@@ -12,6 +12,27 @@ const aboutContainer = document.querySelector(".about-container");
 const servicesContainer = document.querySelector("#services");
 const talkContainer = document.querySelector("#talk");
 
+const wholeContainer = document.querySelector("#hide-on-click")
+const hamburgerButton = document.querySelector(".hamburger-button")
+const hamburgerMenu = document.querySelector("#hamburger-menu")
+const progressBar = document.querySelector("#progress-bar")
+
+hamburgerButton.addEventListener("click", (e) =>{
+    wholeContainer.classList.toggle("display");
+    hamburgerMenu.classList.toggle("displayNav");
+    hamburgerButton.classList.toggle("fa-bars");
+    hamburgerButton.classList.toggle("fa-times");
+    progressBar.classList.toggle("display");  
+})
+
+removeDisplayNone = () => {
+    wholeContainer.classList.toggle("display");
+    progressBar.classList.toggle("display");  
+    hamburgerMenu.classList.toggle("displayNav");
+    hamburgerButton.classList.toggle("fa-bars");
+    hamburgerButton.classList.toggle("fa-times");
+}
+
 /*function fadeInOnScroll(){
     const scrollNum = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = window.scrollY;
