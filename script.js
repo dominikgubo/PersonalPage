7331f2627a9aa772fb76b50c14b07bdb90f37b57
Gubinator <gubo.dominik@gmail.com>
2022-06-27 11:21:05 +0200

    UX/UI changes

--- script.js
+++ script.js
@@ -32,28 +32,3 @@ removeDisplayNone = () => {
     hamburgerButton.classList.toggle("fa-bars");
     hamburgerButton.classList.toggle("fa-times");
 }
-
-/*function fadeInOnScroll(){
-    const scrollNum = document.documentElement.scrollHeight - window.innerHeight;
-    const scrolled = window.scrollY;
-    console.log(scrolled);
-
-    const{scrollTop, scrollHeight}=document.documentElement;
-    const scrollPercent = `${(scrollTop / (scrollHeight-window.innerHeight))*100}%`;
-    const scrollDecimal = (scrollTop / (scrollHeight-window.innerHeight))*100;
-    console.log(scrollDecimal);
-    if(scrollDecimal >= 0.3 &&  scrollDecimal < 0.4){
-        aboutContainer.classList.add("animation");
-        aboutContainer.removeAttribute("hidden");
-    }
-    if(scrollDecimal >= 0.6 &&  scrollDecimal < 0.7){
-        servicesContainer.classList.add("animation");
-        servicesContainer.removeAttribute("hidden");
-    }
-    if(scrollDecimal >= 0.8 &&  scrollDecimal < 0.9){
-        talkContainer.classList.add("animation");
-        talkContainer.removeAttribute("hidden");
-    }
-}
-
-document.addEventListener("scroll", fadeInOnScroll);*/
