cd97698aa55d1d6fbb59ffc86ceab37c45a249c6
Dominik Gubo <dominik.gubo2@gmail.com>
2022-08-12 15:28:20 +0200

    Added scroll animations

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+AOS.init();
+
 function updateProgressBar(){
     const{scrollTop, scrollHeight}=document.documentElement;
     const scrollPercent = `${(scrollTop / (scrollHeight-window.innerHeight))*100}%`;
